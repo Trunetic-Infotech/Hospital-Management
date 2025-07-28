d06b60c8ba86df9788fcb9e42220044fca62b19e
AmarKalel077 <AmaR.k@truneticinfotech.com>
2025-07-28 10:31:42 +0530

    Navbar Updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import img1 from "../assets/homeimage3.jpg";
 import logo1 from "../assets/logo.png";
-=======
-import React from 'react'
-import img1 from "../assets/health.png"
-import logo1 from "../assets/logo.png"
-import icon1 from "../assets/icons8-admin-50.png"
-import image from "../assets/background.jpg"
 import img2 from "../assets/home_page.jpg"
->>>>>>> 270bfdc374b8aea2385609e8cbc9ab3ac3d8c8dd
 
-
-import Navbar from "../Components/Navbar";
 
 const Home = () => {
-<<<<<<< HEAD
  
-=======
+
   return (
    <div className="w-full">
   <img
@@ -26,45 +15,6 @@ const Home = () => {
     className="w-full h-auto block"
   />
 </div>
-  ) 
-}
->>>>>>> 270bfdc374b8aea2385609e8cbc9ab3ac3d8c8dd
-
-  return (
-    <div className="relative h-screen w-full bg-white overflow-auto">
-     
-
-      <Navbar/>
-
-      {/* Main Home Content (Always Visible) */}
-      <div className="flex justify-center p-10">
-        {/* Left Side */}
-        <div className="w-[50%] mx-40 flex-col justify-center">
-          <img src={logo1} className="w-[400px] h-[350px]" />
-          <div className="font-bold text-5xl text-black leading-snug tracking-wide">
-            <p>A heritage in care.</p>
-            <p>A reputation in</p>
-            <p>excellence</p>
-          </div>
-          <div className="w-[90%] my-10">
-            <p className="text-xl text-black tracking-wide leading-relaxed font-serif">
-              MedCare Hospital provides excellent service by prioritizing the
-              safety and security of patients
-            </p>
-          </div>
-        </div>
-
-        {/* Right Side */}
-        <div>
-          <img
-            src={img1}
-            className="w-[700px] h-[500px] my-20 rounded-[100px]"
-          />
-        </div>
-      </div>
-
-     
-    </div>
   );
 };
 
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,67 +1,233 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx"; // your preferred icons
-import { Link } from 'react-router-dom';
-import img1 from"../assets/medcare.png"
+import { Link } from "react-router-dom";
+import img1 from "../assets/medcare.png";
+
+import {
+  ShieldCheck,
+  Stethoscope,
+  HeartPulse,
+  ClipboardList,
+  X,
+  AlertTriangle,
+} from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const navigate = useNavigate();
+
+  const roles = [
+    { label: "Admin", icon: ShieldCheck, color: "text-black", role: "Admin" },
+    { label: "Doctor", icon: Stethoscope, color: "text-black", role: "Doctor" },
+    {
+      label: "Patient",
+      icon: HeartPulse,
+      color: "text-black",
+      role: "Patient",
+    },
+    {
+      label: "Receptionist",
+      icon: ClipboardList,
+      color: "text-red-400 hover:text-white",
+      role: "receptionist",
+    },
+    {
+      label: "Room & Beds",
+      icon: ClipboardList,
+      color: "text-black",
+      role: "room-beds",
+    },
+    {
+      label: "Appointments",
+      icon: ClipboardList,
+      color: "text-black",
+      role: "appointments",
+    },
+    {
+      label: "Billing & Payments",
+      icon: ClipboardList,
+      color: "text-black",
+      role: "billing",
+    },
+    {
+      label: "Reports",
+      icon: ClipboardList,
+      color: "text-black",
+      role: "reports",
+    },
+    {
+      label: "Labrotary",
+      icon: ClipboardList,
+      color: "text-black",
+      role: "Labrotary",
+    },
+    // {
+    //   label: "Complaints",
+    //   icon: ClipboardList,
+    //   color: "text-black",
+    //   role: "complaints",
+    // },
+    // {
+    //   label: "Inventory",
+    //   icon: ClipboardList,
+    //   color: "text-black",
+    //   role: "inventory",
+    // },
+    // {
+    //   label: "Shift & Schedule",
+    //   icon: ClipboardList,
+    //   color: "text-black",
+    //   role: "shift-schedule",
+    // },
+    {
+      label: "Emergency Alert",
+      icon: AlertTriangle,
+      color: "text-red-500 hover:text-white",
+      role: "Emergency-Alert",
+    },
+    // {
+    //   label: "Attendance",
+    //   icon: ClipboardList,
+    //   color: "text-black",
+    //   role: "attendance",
+    // },
+  ];
 
   return (
-    <div className='w-full bg-green-100'>
-      <div className='flex flex-wrap items-center justify-between px-4 lg:px-8 py-2'>
-
+    <div className="w-full bg-green-100">
+      <div className="flex flex-wrap items-center justify-between px-4 lg:px-8 py-2">
         {/* Logo Section */}
         <div className="flex items-center h-[80px] px-4 bg-white">
-  <img
-    src={img1}
-    alt="logo"
-    className="h-full w-auto object-contain"
-  />
-</div>
-
+          <img src={img1} alt="logo" className="h-full w-auto object-contain" />
+        </div>
 
         {/* Hamburger Icon (only on small screens) */}
-        <div className='lg:hidden'>
-          <button onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle menu">
+        <div className="lg:hidden">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            aria-label="Toggle menu"
+          >
             {menuOpen ? <RxCross1 size={28} /> : <RxHamburgerMenu size={28} />}
           </button>
         </div>
 
         {/* Desktop Menu */}
-        <div className='hidden lg:flex lg:items-center lg:space-x-8'>
+        <div className="hidden lg:flex lg:items-center lg:space-x-8">
           <nav>
-            <div className='flex my-4'>
-              <Link to="/" className='text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E] hover:-translate-y-1 hover:shadow-xl hover:scale-105'>Home</Link>
-              <Link to="/AboutPage" className='text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E]  hover:-translate-y-1 hover:shadow-xl hover:scale-105'>About</Link>
-              <Link to="/Contact" className='text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E]  hover:-translate-y-1 hover:shadow-xl hover:scale-105'>Contact</Link>
-              <Link to="/BlogsPage" className='text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E]  hover:-translate-y-1 hover:shadow-xl hover:scale-105'>Blog</Link>
+            <div className="flex my-4">
+              <Link
+                to="/"
+                className="text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E] hover:-translate-y-1 hover:shadow-xl hover:scale-105"
+              >
+                Home
+              </Link>
+              <Link
+                to="/AboutPage"
+                className="text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E]  hover:-translate-y-1 hover:shadow-xl hover:scale-105"
+              >
+                About
+              </Link>
+              <Link
+                to="/Contact"
+                className="text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E]  hover:-translate-y-1 hover:shadow-xl hover:scale-105"
+              >
+                Contact
+              </Link>
+              <Link
+                to="/BlogsPage"
+                className="text-green-950 my-2 mx-4 font-semibold text-xl hover:text-[#0F766E]  hover:-translate-y-1 hover:shadow-xl hover:scale-105"
+              >
+                Blog
+              </Link>
             </div>
           </nav>
-          <div className='flex'>
-            <button className='bg-[#0F766E] text-white w-[100px] h-[50px] mx-2 rounded-xl text-lg font-semibold'>Login</button>
-            <button className='bg-[#0F766E] text-white w-[100px] h-[50px] mx-2 rounded-xl text-lg font-bold'>Register</button>
+          <div className=" flex  mt-4 gap-2    ">
+            <button
+              onClick={() => setShowSidebar(true)}
+              className="bg-[#0F766E] text-white w-full h-[45px] rounded-xl text-base font-semibold px-4
+"
+            >
+              Login
+            </button>
+            <button className="bg-[#0F766E] text-white w-full h-[45px] rounded-xl text-base font-semibold px-4
+">
+              Register
+            </button>
           </div>
         </div>
       </div>
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className='lg:hidden px-4 pb-4'>
+        <div className="lg:hidden px-4 pb-4">
           <nav>
-            <ul className='flex flex-col gap-2'>
-              <li className='text-green-950 font-semibold text-lg hover:text-[#0F766E]'>Home</li>
-              <li className='text-green-950 font-semibold text-lg hover:text-[#0F766E]'>About</li>
-              <li className='text-green-950 font-semibold text-lg hover:text-[#0F766E]'>Services</li>
-              <li className='text-green-950 font-semibold text-lg hover:text-[#0F766E]'>Contact</li>
+            <ul className="flex flex-col gap-2">
+              <li className="text-green-950 font-semibold text-lg hover:text-[#0F766E]">
+                Home
+              </li>
+              <li className="text-green-950 font-semibold text-lg hover:text-[#0F766E]">
+                About
+              </li>
+              <li className="text-green-950 font-semibold text-lg hover:text-[#0F766E]">
+                Services
+              </li>
+              <li className="text-green-950 font-semibold text-lg hover:text-[#0F766E]">
+                Contact
+              </li>
             </ul>
           </nav>
-          <div className='flex flex-col mt-4 gap-2'>
-            <button className='bg-[#0F766E] text-white w-full h-[45px] rounded-xl text-base font-semibold'>Login</button>
-            <button className='bg-[#0F766E] text-white w-full h-[45px] rounded-xl text-base font-bold'>Register</button>
+          <div className="flex flex-col mt-4 gap-2">
+            <button className="bg-[#0F766E] text-white w-full h-[45px] rounded-xl text-base font-semibold">
+              Login
+            </button>
+            <button className="bg-[#0F766E] text-white w-full h-[45px] rounded-xl text-base font-bold">
+              Register
+            </button>
           </div>
         </div>
       )}
+
+
+        {showSidebar && (
+        <div className="absolute top-1 right-1 h-[85vh] w-72 bg-gradient-to-bl from-emerald-700 via-teal-500 to-white text-black flex flex-col p-6 shadow-2xl rounded-2xl border border-blue-300 z-50 transition-all duration-300 ease-in-out">
+          {/* Close Button */}
+          <div className="flex justify-end mb-2">
+            <button
+              onClick={() => setShowSidebar(false)}
+              className="hover:text-red-600 transition"
+            >
+              <X size={26} />
+            </button>
+          </div>
+
+          <h2 className="text-3xl font-bold mb-8 text-center text-white">
+            Login
+          </h2>
+
+          <div className="flex flex-col gap-4 ">
+            {roles.map((role, index) => (
+              <button
+                key={index}
+                onClick={() => navigate(`/login/${role.role}`)}
+                className={`flex items-center gap-4 px-4 py-3 rounded-xl bg-white hover:bg-gradient-to-r hover:from-cyan-500 hover:to-emerald-400 text-white hover:text-black transition-all duration-300 shadow-md ${role.color}`}
+              >
+                <role.icon
+                  size={22}
+                  className="transition-transform group-hover:scale-110 text-black"
+                />
+                <span className="text-lg font-medium text-black">
+                  {role.label}
+                </span>
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+
     </div>
   );
 };
